8135afe4dd65b230bc7cd5163917ea9386e26c7d
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-11 15:51:52 +0900

    野球チーム一覧ファイルを作成

---
+++ src/views/Ex01.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ex01">
+    <h1>野球チーム一覧</h1>
+    <div v-for="team of teams" v-bind:key="team">
+      <router-link :to="'/ex01TeamDetail/' + team.id">{{
+        team.teamName
+      }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Team } from "@/types/team";
+
+@Component
+export default class Ex01 extends Vue {
+  get teams() {
+    return this["$store"].getters.getTeams;
+  }
+}
+</script>
+
+<style></style>
